826766cb63ba398648cf38ac42a9fdddee8a51db
biningo <biningo.cn@gmail.com>
2024-04-14 15:47:08 +0800

    fix: typo

--- src/test/java/loadbalance/ThreadUtil.java
+++ src/test/java/loadbalance/ThreadUtil.java
@@ -6,11 +6,11 @@ import java.util.concurrent.TimeUnit;
 public class ThreadUtil {
     public static void waitTasks(ThreadPoolExecutor executor) {
         while (executor.getActiveCount() > 0 || !executor.getQueue().isEmpty()) {
-            ThreadUtil.sleepIgnoreInterceptor(10, TimeUnit.MILLISECONDS);
+            ThreadUtil.sleepIgnoreIntercept(10, TimeUnit.MILLISECONDS);
         }
     }
 
-    public static void sleepIgnoreInterceptor(int time, TimeUnit timeUnit) {
+    public static void sleepIgnoreIntercept(int time, TimeUnit timeUnit) {
         try {
             Thread.sleep(timeUnit.toMillis(time));
         } catch (InterruptedException ignored) {
